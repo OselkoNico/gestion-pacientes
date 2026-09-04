18134abdd7b84941fcd860ff7d75ca5c36936070
Osel Nicolás <oselko123@gmail.com>
2026-09-04 23:11:43 +0200

    Añadir Backend API Rest

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import router from './routes/pancientes';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/pacientes', router);
+
+app.use((req,res) => {
+    res.status(404).json({
+        message: 'Incorrect route or params.'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
---
+++ Backend/routes/pancientes.js
@@ -0,0 +1,89 @@
+import express from 'express';
+const router = express.Router();
+
+const pacientes = [];
+
+router.get('/', (req, res) => {
+    res.status(200).json(pacientes);
+});
+
+router.get('/:dni', (req, res) => {
+    const persona = pacientes.find(
+        paciente => paciente.dni === req.params.dni
+    );
+
+    if(!persona) {
+        return res.status(404).json({
+            message: 'DNI no encontrado.'
+        });
+    }
+
+    res.status(200).json(persona);
+});
+
+router.post('/', (req, res) => {
+    if(!req.body.dni) {
+        return res.status(400).json({
+            message: 'Paciente data mandatory'
+        });
+    }
+
+    const existingPaciente = pacientes.find(
+        paciente => paciente.dni === req.body.dni
+    );
+
+    if(existingPaciente) {
+        return res.status(400).json({
+            message: 'DNI already exists.'
+        })
+    }
+
+    pacientes.push(req.body);
+
+    res.status(201).json(req.body);
+});
+
+router.put('/:dni', (req, res) => {
+    if(!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).json({
+            message: 'Paciente data or cif param mandatory'
+        });
+    }
+
+    const pacienteIndex = pacientes.findIndex(
+        paciente => paciente.dni === req.params.dni
+    );
+
+    if(pacienteIndex < 0) {
+        return res.status(404).json({
+            message: 'Paciente not found with that DNI'
+        });
+    }
+
+    const { dni, ...updateData } = req.body;
+
+    pacientes[pacienteIndex] = {
+        ...pacientes[pacienteIndex],
+        ...updateData
+    };
+
+    res.status(200).json(pacientes[pacienteIndex]);
+});
+
+router.delete('/:dni', (req, res) => {
+    const pantienteIndex = pacientes.findIndex(
+        paciente => paciente.dni === req.params.dni
+    );
+
+    if(pacienteIndex < 0) {
+        return res.status(404).json({
+            message: 'Paciente not found with that DNI'
+        });
+    }
+
+    const [deletedPaciente] = pacientes.splice(pacienteIndex, 1);
+
+    res.status(200).json(deletedPaciente);
+});
+
+export default router;
